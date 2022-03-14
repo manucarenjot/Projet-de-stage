e97a98574c29d4d96ed83531077c841e1b34940d
manucarenjot <carenjotje@gmail.com>
2022-03-14 14:10:30 +0100

    redirection des pages principale terminées

--- View/public/home.html.php
+++ View/public/home.html.php
@@ -1,1 +1,47 @@
 <h1>Services</h1>
+
+<div class="services">
+    <h3>Redressage</h3>
+    <p>Mise au marbre , redressage et contrôle des châssis.</p>
+</div>
+
+<div class="services">
+    <h3>Peinture tout supports</h3>
+    <p>Mise en peinture de tout élement : moto, voiture, camion, tracteur, scooter, vélo, radiateur de chauffage
+        centrale,... n'hésitez pas à nous contactez pour toute autre demande</p>
+    <div>
+    </div>
+
+    <div class="services">
+        <h3>Tôlerie</h3>
+        <p>Remise en forme et soudure d' élément de carrosserie en acier et en aluminium</p>
+    </div>
+
+    <div class="services">
+        <h3>Polissage de luxe</h3>
+        <p>Redonnez l'aspect du neuf a votre carrosserie et votre intérieur , ceci par nettoyage , polissage et lustrage
+            de votre véhicule !</p>
+    </div>
+
+    <div class="services">
+        <h3>Vitres teintées</h3>
+        <p>Teintez vos vitres et protégez-vous du soleil, évitez les regard indiscret et donné un look sympa a votre
+            voiture!</p>
+    </div>
+
+
+    <form method="post" action="">
+        <br>
+        <div class="add-service">
+            <label for="title">Titre :</label>
+            <br>
+            <input type="text" name="title" id="title">
+        </div>
+        <div class="add-service">
+            <label for="description">Description :</label>
+            <br>
+            <textarea name="description" id="description" cols="60" rows="5"></textarea>
+        </div>
+        <input type="submit" name="send" id="send" value="&#10004;">
+
+    </form>
