20c8b497aece5117804017d40300696bf50bcbf3
manucarenjot <carenjotje@gmail.com>
2022-03-15 11:51:49 +0100

    Construction du code html des pages -En cours
      -> redirection de la page admin si l'utilisateur est pas connecté

--- Models/manager/AdminManager.php
+++ Models/manager/AdminManager.php
@@ -5,17 +5,20 @@ use App\entity\Admin;
 
 class AdminManager extends Admin
 {
-    public static function connectUserWithMail(string $mail)
+    public static function connectUserWithMail(string $mail, string $password)
     {
         $result = Connect::getPDO()->prepare("SELECT * FROM ftk09_admin WHERE mail = '{$mail}'");
 
         $result->execute();
         $admin = $result->fetch();
         if ($admin) {
-            echo 'l\'adresse est valide';
+            echo 'l\'adresse est valide ';
+            if (password_verify($password, $admin['password'])) {
+                session_start();
+                $_SESSION['admin'] = $admin;
         }
         else {
-            echo 'adresse invalide';
+            echo '<p class="alert error-alert"><strong>Adresse mail invalide !</strong></p>';
         }
 
     }
