ade3b704aad81cef72ece9e830d15ceec2452cec
manucarenjot <carenjotje@gmail.com>
2022-03-15 15:36:37 +0100

    Function addService et readServicesDB terminé
    A faire
      -> sécuriser les champs

--- Controller/HomeController.php
+++ Controller/HomeController.php
@@ -9,11 +9,12 @@ class HomeController extends AbstractController
     public function index()
     {
         $this->render('public/home');
+        ServicesManager::readServiceDB();
         if ($this->getPost()) {
             //TODO verifier les champs !!!
 
             $title = trim(strip_tags($_POST['title']));
-            $description = trim(strip_tags($_POST['description']));
+            $description = trim(htmlentities($_POST['description']));
 
             ServicesManager::addService($title, $description);
         }
--- Models/manager/ServicesManager.php
+++ Models/manager/ServicesManager.php
@@ -4,10 +4,33 @@ use App\Connect\Connect;
 
 class ServicesManager
 {
-    public static function addService(string $title, string $description) {
-        $add = Connect::getPDO()->prepare("INSERT INTO ftk09_services (title, description) values ('{$title}', '{$description}')");
+public static function addService(string $title, string $description)
+{
+    $add = Connect::getPDO()->prepare("INSERT INTO ftk09_services (title, description) values (:title, :description)");
 
-        $add->execute();
+    $add->bindValue(':title', $title);
+    $add->bindValue(':description', $description);
 
+    if ($add->execute()) {
+        header('LOCATION: ?c=home');
     }
+
 }
+
+public static function readServiceDB() {
+$select = Connect::getPDO()->prepare("SELECT * FROM ftk09_services");
+
+$select->execute();
+if ($select->execute()) {
+foreach ($datas = $select->fetchAll() as $data) { ?>
+<div>
+    <h3><?= $data['title'] ?></h3>
+    <p><?= $data['description'] ?></p>
+</div>
+<?php
+}
+
+}
+}
+}
+
