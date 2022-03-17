81cdba7500bb5d539984010021207310be8d3282
manucarenjot <carenjotje@gmail.com>
2022-03-17 14:42:05 +0100

    function getMessage et getMessageById terminé

--- Models/manager/MessagerieManager.php
+++ Models/manager/MessagerieManager.php
@@ -28,8 +28,8 @@ class MessagerieManager
                 ?>
                 <p>_</p><br>
                 <div class="coordonner">
-                    <a href="?c=espace-admin&a=messages&id=<?= $data['id'] ?>"><?= $data['name'] ?> <?= $data['firstname'] ?>
-                        <br> <?= $data['date'] ?></a><br><br>
+                    <a href="?c=espace-admin&a=messages&id=<?= $data['id'] ?>">- <?= $data['name'] ?>  <?= $data['firstname'] ?>
+                        <br></a><br><br>
                 </div>
                 <?php
                 }
@@ -47,13 +47,12 @@ class MessagerieManager
         if ($select->execute()) {
             foreach ($datas = $select->fetchAll() as $data) { ?>
                 <div class="message">
-                    <p class="name"><?= $data['name'] ?></p>
-                    <p class="firstname"><?= $data['firstname'] ?></p>
-                    <p
-                            class="date"><?= $data['date'] ?></p>
-                    <p class="mail"><?= $data['mail'] ?></p>
-                    <p class="phone"><?= $data['phone'] ?></p>
-                    <p class="content"><?= $data['messages'] ?></p>
+                    <p class="name">Nom : <?= $data['name'] ?></p>
+                    <p class="firstname"> Prenom : <?= $data['firstname'] ?></p>
+                    <p class="date"> Envoyé le : <?= $data['date'] ?></p>
+                    <p class="mail"> E-mail <?= $data['mail'] ?></p>
+                    <p class="phone">Numéro de téléphone : <?= $data['phone'] ?></p>
+                    <p class="content">Message : <br><br><?= $data['messages'] ?></p>
                 </div>
                 <?php
             }
