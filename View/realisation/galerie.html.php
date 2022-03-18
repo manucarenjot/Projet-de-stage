49ad94193f9666c7f3e9feda256e79d38444343e
manucarenjot <carenjotje@gmail.com>
2022-03-18 15:27:47 +0100

    debut du css et debut de la galerie

--- View/realisation/galerie.html.php
+++ View/realisation/galerie.html.php
@@ -1,1 +1,62 @@
 <h1>Réalisations</h1>
+
+<?php
+
+function getRandomName(string $regularName) {
+    $infos = pathinfo($regularName);
+    try {
+        $bytes = random_bytes(15);
+    }
+    catch (Exception $e) {
+        $bytes = openssl_random_pseudo_bytes(15);
+    }
+    return bin2hex($bytes) . '.' . $infos['extension'];
+}
+
+?>
+
+
+
+
+
+<?php
+
+$files = glob('uploads/*');
+foreach($files as $filename){echo '<img class="gallerieImage" src="'. $filename . ' " height="250"</img>';}
+
+if (isset($_FILES["fichierUtilisateur"])) {
+    $allowedMimeTypes = ['text/plain', 'image/jpeg', 'image/png'];
+    if (in_array($_FILES['fichierUtilisateur']['type'], $allowedMimeTypes)) {
+        if ($_FILES['fichierUtilisateur']['error'] === 0) {
+            $tmp_name = $_FILES['fichierUtilisateur']["tmp_name"];
+
+            $name = getRandomName($_FILES['fichierUtilisateur']["name"]);
+            if (!is_dir('uploads')) {
+                mkdir('uploads', '0755');
+            }
+            move_uploaded_file($tmp_name, 'uploads/' . $name);
+
+            echo '<p class="success">upload réussi !</p><br>';
+            echo $name . '<br>';
+            foreach ($_FILES['fichierUtilisateur'] as $key => $value) {
+                echo "$key => $value <br><br>";
+            }
+        } else {
+            echo '<p>Une erreur s\'est produite lors de l\'upload du fichier!</p>';
+        }
+    } else {
+        echo '<br><p>le type du fichier n\'est pas autorisé !</p><style> p { color: red;
+}
+
+.success {
+color: limegreen;
+}</style>';
+    }
+}
+
+?>
+<form action="?c=realisations" method="post" enctype="multipart/form-data">
+    <label for="id-fichier" id="upfile1" style="cursor:pointer"><i class="fas fa-plus"></i></label>
+    <input type="file" name="fichierUtilisateur" id="id-fichier" style="display: none"/><br>
+    <input type="submit" value="Send"><br>
+</form>
--- public/index.php
+++ public/index.php
@@ -5,15 +5,14 @@ use App\Controller\ErrorController\ErrorController;
 
 session_start();
 
-if(isset($_SESSION['alert']) && count($_SESSION['alert']) > 0) {
+if (isset($_SESSION['alert']) && count($_SESSION['alert']) > 0) {
     $alerts = $_SESSION['alert'];
     unset($_SESSION['alert']);
 
-    foreach($alerts as $alert) { ?>
+    foreach ($alerts as $alert) { ?>
         <div class="alert alert-error"><strong><?= $alert ?></strong></div> <?php
     }
 }
-
 
 
 require __DIR__ . '/../require.php';
@@ -26,16 +25,18 @@ require __DIR__ . '/../require.php';
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="asset/css/style.css">
     <title>Carrosserie Proissy</title>
 </head>
 <body>
-<a href="?c=home">| Acceuil |</a>
-<a href="?c=realisations">Realisation |</a>
-<a href="?c=demande-de-devis">Demande de devis |</a>
-<a href="?c=contact">Contact |</a>
-<a href="?c=espace-admin">Espace admin |</a>
-</body>
-</html>
+<div class="menu">
+    <a href="?c=home">| Acceuil |</a>
+    <a href="?c=realisations">Realisation |</a>
+    <a href="?c=demande-de-devis">Demande de devis |</a>
+    <a href="?c=contact">Contact |</a>
+    <a href="?c=espace-admin">Espace admin |</a>
+</div>
+
 <?php
 
 $page = isset($_GET['c']) ? Router::secureUrl($_GET['c']) : 'home';
@@ -45,27 +46,30 @@ $action = isset($_GET['a']) ? Router::secureUrl($_GET['a']) : 'index';
 switch ($page) {
     case 'home':
         Router::route('HomeController');
-    break;
+        break;
     case 'update-service':
         Router::route('UpdateServiceController');
-    break;
+        break;
     case 'realisations':
         Router::route('RealisationController');
-    break;
+        break;
     case 'demande-de-devis':
         Router::route('DevisController');
-    break;
+        break;
     case 'contact':
         Router::route('ContactController');
-    break;
+        break;
     case 'espace-admin':
         Router::route('AdminController', $action);
-    break;
+        break;
     case 'connect':
         Router::route('ConnectController', $action);
-    break;
+        break;
     default:
         ErrorController::error404($page);
 }
 ?>
+<script src="https://kit.fontawesome.com/d8438e7f2f.js" crossorigin="anonymous"></script>
+</body>
+</html>
 
