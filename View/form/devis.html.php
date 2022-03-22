70f6ad419f0e0750e7907a736be070a87f931390
manucarenjot <carenjotje@gmail.com>
2022-03-22 16:34:42 +0100

    indentation des formulaires

--- View/form/devis.html.php
+++ View/form/devis.html.php
@@ -1,45 +1,4 @@
-<?php
 
-function getRandomName(string $regularName) {
-    $infos = pathinfo($regularName);
-    try {
-        $bytes = random_bytes(15);
-    }
-    catch (Exception $e) {
-        $bytes = openssl_random_pseudo_bytes(15);
-    }
-    return bin2hex($bytes) . '.' . $infos['extension'];
-}
-
-?>
-<?php
-
-if (isset($_FILES["carteGrise"])) {
-    $allowedMimeTypes = ['text/plain', 'image/jpeg', 'image/png'];
-    if (in_array($_FILES['carteGrise']['type'], $allowedMimeTypes)) {
-        if ($_FILES['carteGrise']['error'] === 0) {
-            $tmp_name = $_FILES['carteGrise']["tmp_name"];
-
-            $name = getRandomName($_FILES['carteGrise']["name"]);
-            if (!is_dir('uploads')) {
-                mkdir('uploads', '0755');
-            }
-            move_uploaded_file($tmp_name, 'uploads/' . $name);
-
-            echo '<p class="success">upload réussi !</p><br>';
-            echo $name . '<br>';
-            foreach ($_FILES['fichierUtilisateur'] as $key => $value) {
-                echo "$key => $value <br><br>";
-            }
-        } else {
-            echo '<p>Une erreur s\'est produite lors de l\'upload du fichier!</p>';
-        }
-    } else {
-        echo '<br><p>le type du fichier n\'est pas autorisé !</p>';
-    }
-}
-
-?>
 
 <h1>Demande de devis</h1>
 
